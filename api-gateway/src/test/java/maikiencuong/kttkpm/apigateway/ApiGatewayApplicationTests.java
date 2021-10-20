cbb3d447005ff1df1695b5a9b5c81fd200e6138a
maikiencuong <maikiencuongiuh@gmail.com>
2021-10-21 05:45:21 +0700

    [api-gateway] add api-gateway

---
+++ api-gateway/src/test/java/maikiencuong/kttkpm/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package maikiencuong.kttkpm.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
